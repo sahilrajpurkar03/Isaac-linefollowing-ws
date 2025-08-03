a976ff53b2161f629182ee3b3c5eaf1bff08bd61
Rahul Panchal <112721444+rahulpanchall7@users.noreply.github.com>
2025-08-03 15:57:59 +0000

    removed build log install

--- build/summit_joystick_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rajpurkar/Isaac_Project/Robocon-Lidar-ws/install/summit_joystick_control'
